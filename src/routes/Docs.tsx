228eaf81d0baf4ee03e3e1116586cb60510e3f10
DasLixou <einlixou@gmail.com>
2022-07-14 18:00:11 +0200

    More styling

--- src/routes/Docs.tsx
+++ src/routes/Docs.tsx
@@ -20,9 +20,19 @@ Normal text
 
 const Docs: Component = () => {
   return (
-    <div class="markdown-container bg-white">
-      <SolidMarkdown children={markdown} />
-    </div>
+    <main class="flex h-full">
+      <section class="flex flex-col w-96 h-full bg-slate-100">
+        <div class="flex w-full h-12 bg-slate-100 items-center pl-4">
+          <h2 class="font-inter font-thin">Installation</h2>
+        </div>
+        <div class="flex w-full h-12 bg-slate-50 items-center pl-4 shadow-inner">
+          <h2 class="font-inter font-thin">Getting Started</h2>
+        </div>
+      </section>
+      <div class="markdown-container bg-white w-full h-full">
+        <SolidMarkdown children={markdown} />
+      </div>
+    </main>
   );
 };
 
