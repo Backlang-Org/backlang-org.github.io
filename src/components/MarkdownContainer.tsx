aa3b1021bcf5b43f529b4170e280f1a12e6b449d
DasLixou <einlixou@gmail.com>
2022-07-16 14:39:55 +0200

    Add PrismJS Coloring

--- src/components/MarkdownContainer.tsx
+++ src/components/MarkdownContainer.tsx
@@ -1,9 +1,20 @@
 import { Component } from "solid-js";
 import { marked } from "marked";
+import { highlight, languages } from "prismjs";
 
 interface MarkdownContainerProps {
   content: string | undefined;
 }
+
+marked.setOptions({
+  highlight: (code, lang) => {
+    if (languages[lang]) {
+      return highlight(code, languages[lang], lang);
+    } else {
+      return code;
+    }
+  },
+});
 
 const MarkdownContainer: Component<MarkdownContainerProps> = (
   props: MarkdownContainerProps
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import prismjsPlugin from "vite-plugin-prismjs";
 
 export default defineConfig({
   base: "/",
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    prismjsPlugin({
+      languages: ["bash"],
+      plugins: [],
+      theme: "default",
+      css: true,
+    }),
+  ],
   build: {
     target: "esnext",
     polyfillDynamicImport: false,
