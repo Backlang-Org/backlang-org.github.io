1bee762674ef2a50edd967ae75cd1ad98b4ea8fd
Chris <filmee24@gmail.com>
2022-08-27 14:30:07 +0200

    Cleanup

--- src/routes/GettingStarted.tsx
+++ src/routes/GettingStarted.tsx
@@ -3,8 +3,8 @@ import { Component } from "solid-js";
 const GettingStarted: Component = () => {
   return (
     <main class="overflow-hidden">
-      1. Install <a href="https://dotnet.microsoft.com/en-us/download/dotnet/7.0" target="_blank">.Net 7 Preview</a>
-      2. Download the <a href="https://github.com/Backlang-Org/Backlang/releases" target="_blank">Backlang Compiler binary</a>
+      1. Install <a href="https://dotnet.microsoft.com/en-us/download/dotnet/7.0" target="_blank">.Net 7 Preview</a><br />
+      2. Download the <a href="https://github.com/Backlang-Org/Backlang/releases" target="_blank">Backlang Compiler binary</a><br />
       3. Start coding
     </main>
   );
