513766b68066af40e587147692e78583fb7b157a
Chris <furesoft@users.noreply.github.com>
2022-11-17 08:15:16 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -29,7 +29,7 @@ const Header: Component = () => {
           <h3 class="font-roboto text-2xl font-thin">Getting Started</h3>
         </NavLink>
         <NavLink
-          href="learn"
+          href="learn.backlang.org"
           class="focus:outline-none"
           inactiveClass="focus:outline-none hover:underline"
           activeClass="focus:outline-none underline"
